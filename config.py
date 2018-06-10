04801584e500e5e1a9d205497d06aa47dbbcb596
Alisha <pandhi.alisha@gmail.com>
2018-06-09 21:30:02 -0500

    complete

---
+++ config.py
@@ -0,0 +1,5 @@
+
+consumer_key = "PpstuQwIGHMRVhfXePyYvcXIX"
+consumer_secret = "Km7jYmjncwnklz8c4Sp8G7SmVB5axPlS2U6WWV60ixaCuFXvW3"
+access_token = "519192565-HxNKCOGDPLuFmGJReB0yFUj3Z40EANW62wTGO9ue"
+access_token_secret = "tOencOUiSYy8tqQMhb4mLPqz1gD9qvt8pboZEI8pmm2gi"
